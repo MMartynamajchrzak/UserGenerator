7355525924bedf76a4b885f9bdbfc34a0017d4bd
MM.com <mmartynamajchhrzak@wp.pl>
2021-04-27 00:20:40 +0200

    organize django files

--- App/mydjango/Apps/users/views.py
+++ App/mydjango/Apps/users/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# Create your views here.
 def home_page_view(request):
     return HttpResponse('Hello World!')
--- App/mydjango/Config/urls.py
+++ App/mydjango/Config/urls.py
@@ -1,4 +1,4 @@
-"""mydjango URL Configuration
+"""Config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('djapp.urls')),
+    path('', include('users.urls')),
 ]
