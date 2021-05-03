9bef364987e1c3d688413cc2152f543314fe712d
MM.com <mmartynamajchhrzak@wp.pl>
2021-05-04 00:31:10 +0200

    change sqllite to postres

--- app/config/settings.py
+++ app/config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db_name',
+        'USER': 'db_user',
+        'PASSWORD': 'db_user_password',
+        'HOST': '',
+        'PORT': 'db_port_number',
     }
 }
 
