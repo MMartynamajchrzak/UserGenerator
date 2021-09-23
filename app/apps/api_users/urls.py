a357f262614f596c75b4687533bfd792319d18a1
MM.com <mmartynamajchhrzak@wp.pl>
2021-09-24 01:26:40 +0200

    CR changes

---
+++ app/apps/api_users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from .views import ApiUserViewSet
+
+url_patterns = [
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("register/", ApiUserViewSet.as_view({'post': 'create'}), name='register_user'),
+]
--- app/apps/users/admin.py
+++ app/apps/users/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import User, ApiUser
+from .models import User
 
 admin.site.register(User)
-admin.site.register(ApiUser)
