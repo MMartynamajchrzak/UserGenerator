7dd2edcd42b4f31a260fbcf3d42fa198947e93b6
MM.com <mmartynamajchhrzak@wp.pl>
2021-10-05 22:17:57 +0200

    CR changes

--- app/apps/api_users/apps.py
+++ app/apps/api_users/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ApiUsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api_users'
+    name = 'apps.api_users'
--- app/apps/api_users/models.py
+++ app/apps/api_users/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-from apps.users.models import CreatedUpdatedMixin
+
+class CreatedUpdatedMixin:
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class ApiUser(AbstractUser, CreatedUpdatedMixin):
